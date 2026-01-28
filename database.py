8bba1eb37a1ef7d69807598a314f960696ca8a49
Binyomin <binyomin927@gmail.com>
2026-01-28 19:59:25 +0500

    added autocreate id , created_at and updated_at in models

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncSession,async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import Mapped, mapped_column, declared_attr
+from sqlalchemy import Integer, func
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///database.db"
 
@@ -9,6 +12,14 @@ engine = create_async_engine(DATABASE_URL)
 
 class BaseModel(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + 's'
 
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
